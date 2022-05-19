9dc57a467add6b78e0fccd59eb31c86695d6fab9
can <cnunlu@gmail.com>
2022-05-19 16:21:50 +0300

    DbContext added

---
+++ OnionArchitecture.Persistence/Context/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace OnionArchitecture.Persistence.Context
+{
+    public abstract class DesignTimeDbContextFactory<TContext> : IDesignTimeDbContextFactory<TContext> where TContext : DbContext
+    {
+        protected abstract TContext CreateNewInstance(DbContextOptions<TContext> options);
+        public TContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<TContext> builder = new DbContextOptionsBuilder<TContext>();
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../OnionArchitecture.WebAPI"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+            builder.UseSqlServer(configuration.GetConnectionString("SQLConnection"));
+            return CreateNewInstance(builder.Options);
+        }
+    }
+}

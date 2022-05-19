6dbd49b83db901ac197c91d9457290fe3cec2d22
can <cnunlu@gmail.com>
2022-05-19 15:10:15 +0300

    classes completed

--- OnionArchitecture.Application/ServiceRegistration.cs
+++ OnionArchitecture.Application/ServiceRegistration.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnionArchitecture.Application
+﻿namespace OnionArchitecture.Application
 {
-    internal class ServiceRegistration
+    public static class ServiceRegistration
     {
     }
 }
---
+++ OnionArchitecture.Persistence/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using OnionArchitecture.Persistence.Repositories;
+
+namespace OnionArchitecture.Persistence.UnitOfWorks
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        Task<IDbContextTransaction> BeginTransactionAsync();
+        public IProductRepository ProductRepository { get; }
+    }
+}
